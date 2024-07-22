9fe99e54f45e6c1b876a5f9c712b66e449484967
ishandhanani <ishandhanani@gmail.com>
2024-07-22 16:35:19 -0700

    update script

--- assets/setup-nim.sh
+++ assets/setup-nim.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 set -euo pipefail
 set -x
+
 # Log in to NGC
 echo "${NGC_API_KEY}" | docker login nvcr.io -u '$oauthtoken' --password-stdin
+
 # Set path to your LoRA model store
 export LOCAL_PEFT_DIRECTORY="$(pwd)/loras"
 mkdir -p $LOCAL_PEFT_DIRECTORY
 pushd $LOCAL_PEFT_DIRECTORY
+
 # downloading NeMo-format loras from NGC
 # ngc registry model download-version "nim/meta/llama3-8b-instruct-lora:nemo-math-v1"
 # ngc registry model download-version "nim/meta/llama3-8b-instruct-lora:nemo-squad-v1"
 popd
 chmod -R 777 $LOCAL_PEFT_DIRECTORY
+
 # copy the trained lora to the lora directory
 mkdir -p $LOCAL_PEFT_DIRECTORY/llama3-8b-pubmed-qa
 cp ./results/Meta-Llama-3-8B-Instruct/checkpoints/megatron_gpt_peft_lora_tuning.nemo $LOCAL_PEFT_DIRECTORY/llama3-8b-pubmed-qa
+
 # Set up NIM cache directory
 mkdir -p $HOME/.nim-cache
+
 # Set environment variables
 export NIM_PEFT_SOURCE=/home/nvs/loras
 export CONTAINER_NAME=meta-llama3-8b-instruct
 export NIM_CACHE_PATH=$HOME/.nim-cache
+
 # Run the Docker container
 docker run -d --rm --name=$CONTAINER_NAME \
     --runtime=nvidia \
@@ -30,6 +37,7 @@ docker run -d --rm --name=$CONTAINER_NAME \
     -e NGC_API_KEY \
     -e NIM_PEFT_SOURCE \
     -e NIM_PEFT_REFRESH_INTERVAL \
+    -v $LOCAL_PEFT_DIRECTORY:$NIM_PEFT_SOURCE \
     -v $HOME/.nim-cache:/home/user/.nim-cache \
     nvcr.io/nim/meta/llama3-8b-instruct:1.0.0
 
